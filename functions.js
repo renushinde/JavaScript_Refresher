5b19adee65db440e8dcb5012ee668b3c3176ef63
Renu <renunikhilp@gmail.com>
2020-12-19 11:46:04 -0800

    function basics

---
+++ functions.js
@@ -0,0 +1,18 @@
+// write a function that returns true if a random number is over .5 - otherwise the function should return false. 
+function isOverPointFive () {
+    if (Math.random() > 0.5) {
+        return true;
+    } 
+        return false;
+    
+}
+
+console.log (isOverPointFive());
+
+// Write a function called myName that logs your full name. Save your full name to a variable inside of the function body, then use console.log to print your name to the console.
+
+function myName () {
+    return "Renu";
+}
+
+console.log(myName());
