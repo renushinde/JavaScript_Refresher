e6f85b7c8a087b27255cdf6e79382400a560b43a
Renu <renunikhilp@gmail.com>
2020-12-20 21:57:50 -0800

    scopes

---
+++ functionsExercises.js
@@ -0,0 +1,12 @@
+// 1. Make a function for add, subtract, multiply, and divide. Each of these functions should accept two parameters and return the sum, difference, product and quotient.
+
+function multiply (a, b) {
+    return a*b;
+
+}
+console.log ("The multiplication is: " + multiply(2,2)); 
+
+function add (a,b) {
+    return a+b;
+}
+console.log ("The addition is: " + add(2,2)); 
