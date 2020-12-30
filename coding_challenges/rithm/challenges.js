cbd5cb638bf96ca1f10e1a77dd0b8a7989fe98e1
Renu <renunikhilp@gmail.com>
2020-12-30 10:57:35 -0800

    Implent slice function

--- coding_challenges/rithm/challenges.js
+++ coding_challenges/rithm/challenges.js
@@ -126,3 +126,18 @@ function min ( arr ) {
    }
    
    min([5, 1, 4, 7, 1, 2]);
+
+// Write a function called slice, which accepts an array, and two numbers.The function should return a new array with the elements starting at the index of the first number and going until the index of the second number.If a third parameter is not passed to the function, it should slice until the end of the array by default.If the third parameter is greater than the length of the array, it should slice until the end of the array.Do not use the built in Array.slice() function!
+
+function slice (arr, begin, end) {
+    let newArr = [];
+    if ( end === undefined || end > arr.length) {
+      end = arr.length;
+    }
+    for ( let i = begin; i < end; i ++) {
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  }
+  
+  slice([1, 2, 3, 4, 5], 0, 2);
