09593f5fd174b196ad2a6a18eb4517697fc85af0
Renu <renunikhilp@gmail.com>
2021-02-10 10:28:14 -0800

    Hackerrank day 2

--- coding_challenges/HackerRank/script.js
+++ coding_challenges/HackerRank/script.js
@@ -14,3 +14,32 @@ function vowelsAndConsonants(s) {
 }
 
 vowelsAndConsonants("javascriptloops");
+
+// Prob 2 -
+ function areaOfRectangle (length, width) {
+        let area =  length * width; 
+        let perimeter = 2 * (length + width)
+        return area;
+ }
+console.log( "The area of a rectangle is : " + areaOfRectangle( 5 , 5));
+
+// Prob 3 - Factorial
+
+function factorial (num) {
+    if ( num === 0 || num === 1) {
+        return 1;
+    } else {
+        return num * (factorial(num - 1 ));
+    }
+
+}
+console.log("The factorial is " + factorial(4));
+
+// prob 4 - Calculate area and perimeter of a circle
+
+function calculateAreaOfCircle (radius) {
+    const PI = Math.PI;
+    let area = PI * radius* radius;
+    return area;
+}
+console.log(calculateAreaOfCircle (2.6));
